db18356273931885c2fbdd1ec0bbe6f5cf30439e
Martin Schnabl <martin@schnabl.sc>
2020-04-26 11:08:13 +0200

    Update islandwar_levels.py

--- islandwar_levels.py
+++ islandwar_levels.py
@@ -20,9 +20,9 @@ levels = {#"Levelnr":{"Islandname":[((pos1), (Color1),ships, ((pos2), (Color2),S
                 "Iron_islands":[((150,-200),(0,255,0),0)],                          #..M..S..eM..
                 "Wood_islands":[((150,-600),(0,255,0),0)],                          #pW..........
                 "Ship_islands":[((715,-400),(0,0,255),0)]},
-           "1":{"Main_islands":[((300,-400),(0,255,0),3),((1130,-400),(255,0,0),3)],#I...........I
-                "Iron_islands":[((150,-200),(0,0,255),0),((1280,-600),(0,0,255),0)],#..M...S..eM.. 
-                "Wood_islands":[((150,-600),(0,0,255),0),((1280,-200),(0,0,255),0)],#W...........W
+           "1":{"Main_islands":[((300,-400),(0,255,0),3),((1130,-400),(255,0,0),3)],#I..........I
+                "Iron_islands":[((150,-200),(0,0,255),0),((1280,-600),(0,0,255),0)],#..M..S..eM.. 
+                "Wood_islands":[((150,-600),(0,0,255),0),((1280,-200),(0,0,255),0)],#W..........W
                 "Ship_islands":[((715,-400),(0,0,255),0)]},
            "2":{"Main_islands":[((200,-400),(0,255,0),5),((1230,-400),(255,0,0),5)],#....W.....
                 "Iron_islands":[((715,-600),(0,0,255),0)],                          #M...S...eM
@@ -36,22 +36,71 @@ levels = {#"Levelnr":{"Islandname":[((pos1), (Color1),ships, ((pos2), (Color2),S
                 "Iron_islands":[((515,-200),(0,0,255),0),((715,-200),(0,0,255),0),((915,-200),(0,0,255),0)],#M...S..eM
                 "Wood_islands":[((515,-600),(0,0,255),0),((715,-600),(0,0,255),0),((915,-600),(0,0,255),0)],#..W.W.W..
                 "Ship_islands":[((715,-400),(0,0,255),0)]},
-           "5":{"Main_islands":[((200,-400),(0,255,0),3),((1230,-400),(255,0,0),3)],#pI... eI.
-                "Iron_islands":[((300,-200),(0,255,0),1),((1130,-200),(255,0,0),1)],#M...S..eM
-                "Wood_islands":[((300,-600),(0,255,0),1),((1130,-600),(255,0,0),1)],#pW....eW.
-                "Ship_islands":[((715,-400),(0,0,255),0)]}
-         }
+           "5":{"Main_islands":[((200,-400),(0,255,0),3),((1230,-400),(255,0,0),3)],#.pI..eI.
+                "Iron_islands":[((300,-200),(0,255,0),1),((1130,-200),(255,0,0),1)],#M..S..eM
+                "Wood_islands":[((300,-600),(0,255,0),1),((1130,-600),(255,0,0),1)],#.pW..eW.
+                "Ship_islands":[((715,-400),(0,0,255),0)]},
+           "6":{"Main_islands":[((200,-400),(0,255,0),3),((1230,-400),(255,0,0),5)],#...I.W...
+                "Iron_islands":[((515,-200),(0,0,255),0),((915,-600),(0,0,255),0)], #M...S..eM
+                "Wood_islands":[((515,-600),(0,0,255),0),((915,-200),(0,0,255),0)], #...W.I...
+                "Ship_islands":[((715,-400),(0,0,255),0)]},
+           "7":{"Main_islands":[((300,-400),(0,255,0),3),((1130,-400),(255,0,0),5)],#...I.W....
+                "Iron_islands":[((615,-200),(0,0,255),0),((815,-600),(0,0,255),0)], #S.M...eM.S
+                "Wood_islands":[((615,-600),(0,0,255),0),((815,-200),(0,0,255),0)], #...W.I....
+                "Ship_islands":[((100,-400),(0,0,255),0),((1330,-400),(0,0,255),0)]},
+           "8":{"Main_islands":[((500,-400),(0,255,0),3),((930,-400),(255,0,0),5)],  #...I.W....
+                "Iron_islands":[((100,-200),(0,0,255),0),((100,-600),(0,0,255),0)],  #S.M...eM.S
+                "Wood_islands":[((1330,-600),(0,0,255),0),((1330,-200),(0,0,255),0)],#...W.I....
+                "Ship_islands":[((100,-400),(0,0,255),0),((1330,-400),(0,0,255),0)]},
+           "9":{"Main_islands":[((300,-400),(0,255,0),3),((1130,-400),(255,0,0),3)],  #..W.S.W...
+                "Iron_islands":[((715,-400),(0,0,255),0)],                            #.M..I..eM.
+                "Wood_islands":[((500,-200),(0,0,255),0),((500,-600),(0,0,255),0),    #..W.S.W...
+                                ((930,-200),(0,0,255),0),((930,-600),(0,0,255),0)],
+                "Ship_islands":[((715,-200),(0,0,255),0),((715,-600),(0,0,255),0)]},
+           "10":{"Main_islands":[((200,-400),(0,255,0),3),((1230,-400),(255,0,0),3)],#.I....W.
+                "Iron_islands":[((300,-200),(0,255,0),1),((300,-600),(0,255,0),1)],  #M..S..eM
+                "Wood_islands":[((1130,-200),(255,0,0),1),((1130,-600),(255,0,0),1)],#.I....W.
+                "Ship_islands":[((715,-400),(0,0,255),0)]},
+           "11":{"Main_islands":[((150,-400),(0,255,0),5),((1030,-400),(255,0,0),1)],#....eW.eI
+                "Iron_islands":[((1030,-600),(255,0,0),1),((1330,-200),(255,0,0),1)],#M...eM.eS
+                "Wood_islands":[((1030,-200),(255,0,0),1),((1330,-600),(255,0,0),1)],#....eI.eW
+                "Ship_islands":[((1330,-400),(255,0,0),1)]},
+           "12":{"Main_islands":[((200,-400),(0,255,0),3),((1230,-200),(255,0,0),5),((1230,-600),(255,165,0),5)],#.I..W.eM  #2 enemies
+                "Iron_islands":[((300,-200),(0,0,255),0),((930,-600),(0,0,255),0)],                              #M..S....
+                "Wood_islands":[((300,-600),(0,0,255),0),((930,-200),(0,0,255),0)],                              #.W..I.eM
+                "Ship_islands":[((715,-400),(0,0,255),0)]},
+           }
 
-level_descriptions = {"-4":["You are the leader of the island with a green ring around it.", "Your aim is to conquer all the enemy islands on the map.", "Click on your island to select it, then on the enemy island to send a ship."],
-                      "-3":["On this map, there are three enemy islands. Conquer every one of them."],
-                      "-2":["You can build more ships in the shipyard.", "For every new ship, you first need 5 pieces of iron and wood."],
-                      "-1":["The enemy can have ships too. If you want to win, you have to defeat them", "and take all their islands."],
-                      "0": ["If you want to win this map, you'll have to collect resources and build more ships.", "Don't let the enemy take the resource islands or the shipyard!"],
-                      "1": ["It is time for your first real battle. Good luck!"],
-                      "2": [],
-                      "3": [],
-                      "4": [],
-                      "5": [],
+level_descriptions = {"-4":"You are the leader of the island with a green ring around it. Your aim is to conquer all the enemy islands on the map. Double click on your island to select it, then on the enemy island to send a ship.",
+                      "-3":"On this map, there are three enemy islands. Conquer every one of them.",
+                      "-2":"You can build more ships in the shipyard. For every new ship, you first need 5 pieces of iron and wood.",
+                      "-1":"The enemy can have ships too. If you want to win, you have to defeat them and take all their islands.",
+                      "0" :"If you want to win this map, you'll have to collect resources and build more ships. Don't let the enemy take the resource islands or the shipyard!",
+                      "1" :"It is time for your first real battle. Good luck!",
+                      "2" :"",
+                      "3" :"",
+                      "4" :"",
+                      "5" :"",
+                      "6" :"",
+                      "7" :"",
+                      "8" :"",
+                      "9" :"There is enough wood on the map, but you will have to fight for the iron.",
+                      "10":"",
+                      "11":"",
+                      "12":"Now you have to fight to enemy empires. Lets hope that they destroy each other instead of us.",
+                      "13":"Not programmed yet",
+                      "14":"Not programmed yet",
+                      "15":"Not programmed yet",
+                      "16":"Not programmed yet",
+                      "17":"Not programmed yet",
+                      "18":"Not programmed yet",
+                      "19":"Not programmed yet",
+                      "20":"Not programmed yet",
+                      "21":"Not programmed yet",
+                      "22":"Not programmed yet",
+                      "23":"Not programmed yet",
+                      "24":"Not programmed yet",
+                      "25":"Not programmed yet",
                       }
                       
 
@@ -60,5 +109,8 @@ level_descriptions = {"-4":["You are the leader of the island with a green ring 
 #
 
 def create_sprites(level):
+    #islands = type_player:[pos,ship
     islands = levels[str(level)]
     return islands
+    #print(Hello world)
+    #levels[level][0]
