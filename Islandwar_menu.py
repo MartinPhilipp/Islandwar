0e2c1c09699b9667e28e5632751f36e0ee29862b
Martin Schnabl <martin@schnabl.sc>
2020-04-26 11:09:16 +0200

    Create Islandwar_menu.py

---
+++ Islandwar_menu.py
@@ -0,0 +1,35 @@
+menu = {"main":       ["Play", "Tutorial", "Levels", "Help", "Credits", "Settings","End the game"],
+        "Tutorial":   ["back", "Tutorial 1", "Tutorial 2", "Tutorial 3", "Tutorial 4", "Tutorial 5"],
+        "Levels":     ["back", "Level 1-4", "Level 5-9", "Level 10-14", "Level 15-19", "Level 20-25"],
+        "Level 1-4":  ["back", "Tutorial", "Level 1", "Level 2", "Level 3", "Level 4"],
+        "Level 5-9":  ["back", "Level 5", "Level 6", "Level 7", "Level 8", "Level 9"], 
+        "Level 10-14":["back", "Level 10", "Level 11", "Level 12", "Level 13", "Level 14"],
+        "Level 15-19":["back", "Level 15", "Level 16", "Level 17", "Level 18", "Level 19"],
+        "Level 20-25":  ["back", "Level 20", "Level 21", "Level 22", "Level 23", "Level 24", "Level 25"],  
+        "Help":       ["back"],
+        "Credits":    ["back", "Ines Schnabl", "Martin Schnabl", ],
+        "Settings":   ["back", "Screenresolution", "Fullscreen"],
+        "Resolution": ["back"],
+        "Fullscreen": ["back", "True", "False"],
+        }
+            
+            
+            
+descr = {"Resume"        :"Resume to the game",
+         "Martin Schnabl":"The programmer of this game.",
+         "Ines Schnabl"  :"Martin's younger sister and responsible for the graphics",
+         "Settings"      :"Change the screenresolution only in the beginning!",
+         "Tutorial 1"    :"An introduction to the basics of the game",
+         "Tutorial 2"    :"More complex management of your ships",
+         "Tutorial 3"    :"Teaches you how to build ships in the shipyard.",
+         "Tutorial 4"    :"Your first encounter with enemy ships.",
+         "Tutorial 5"    :"Teaches you the strategies to win the game.",
+         "Tutorial"      :"Learn everything you need to know to play this game.",
+         }
+
+menu_images = {"Tutorial 42" : "tutorial42",
+               }
+linelength = 20
+history = ["main"]
+cursor = 0
+name = "main"
