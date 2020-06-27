9ddcb77622bbd3f81b3b6b2628424a8389ffed5c
Martin Schnabl <martin@schnabl.sc>
2020-06-27 14:34:52 +0200

    Update credits
    Welcome in the team Marcel!

--- islandwar_menu.py
+++ islandwar_menu.py
@@ -1,23 +1,27 @@
-menu = {"main":       ["Play", "Tutorial", "Levels", "Help", "Credits", "Settings","End the game"],
+menu = {"main":       ["Play", "Tutorial", "Levels", "Campaign", "Help", "Credits", "Settings","End the game"],
         "Tutorial":   ["back", "Tutorial 1", "Tutorial 2", "Tutorial 3", "Tutorial 4", "Tutorial 5"],
         "Levels":     ["back", "Level 1-4", "Level 5-9", "Level 10-14", "Level 15-19", "Level 20-25"],
         "Level 1-4":  ["back", "Tutorial", "Level 1", "Level 2", "Level 3", "Level 4"],
         "Level 5-9":  ["back", "Level 5", "Level 6", "Level 7", "Level 8", "Level 9"], 
         "Level 10-14":["back", "Level 10", "Level 11", "Level 12", "Level 13", "Level 14"],
         "Level 15-19":["back", "Level 15", "Level 16", "Level 17", "Level 18", "Level 19"],
-        "Level 20-25":["back", "Level 20", "Level 21", "Level 22", "Level 23", "Level 24", "Level 25"],  
+        "Level 20-25":["back", "Level 20", "Level 21", "Level 22", "Level 23", "Level 24", "Level 25"],
+        "Campaign":   ["back", "Mission 1", "Mission 2", "Mission 3", "Mission 4", "Mission 5"],
         "Help":       ["back", "Main island", "Wood island", "Iron island", "Ship island",],
-        "Credits":    ["back", "Ines Schnabl", "Martin Schnabl", "Julia Steger"],
+        "Credits":    ["back", "Ines Schnabl", "Martin Schnabl", "Julia Steger","Marcel Korinek"],
         "Settings":   ["back", "Game speed", "Screenresolution", "Fullscreen"],
         "Resolution": ["back"],
         "Fullscreen": ["back", "True", "False"],
         "Game speed": ["back", "Slow", "Normal", "Fast", "Really fast"]
-        }      
+        }
+            
+            
             
 descr = {"Resume"        :"Resume to the game",
          "Martin Schnabl":"The programmer of this game.",
          "Ines Schnabl"  :"Martin's younger sister and responsible for the graphics",
          "Julia Steger"  :"Responsible for game testing, ideas for enhancements and bug detecting",
+         "Marcel Korinek":"Testing.",
          "Settings"      :"Change the screenresolution only in the beginning!",
          "Tutorial 1"    :"An introduction to the basics of the game",
          "Tutorial 2"    :"More complex management of your ships",
@@ -29,6 +33,7 @@ descr = {"Resume"        :"Resume to the game",
          "Wood island"   :"If you capture this island, you will earn wood every second.",
          "Iron island"   :"If you capture this island, you will earn iron every second.",
          "Ship island"   :"On this island there is a shipyard that will produce a new ship whenever you have 5 pieces of wood and 5 ingots of iron.",
+         "Campaign"      :"Not yet written. If you are good at storywriting, please contact us!"
          }
 
 menu_images = {"Main island" : "main_island",
