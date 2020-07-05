af85fff50e2d5727053dc677c2e55b8f4aa5d1a8
Martin Schnabl <martin@schnabl.sc>
2020-07-05 17:26:38 +0200

    Improved German menu

--- islandwar_menu.py
+++ islandwar_menu.py
@@ -11,30 +11,10 @@ menu_e = {"main":       ["Play", "Tutorial", "Levels", "Campaign", "Help", "Cred
         "Credits":    ["back", "Ines Schnabl", "Martin Schnabl", "Julia Steger","Marcel Korinek"],
         "Settings":   ["back", "Language", "Game speed", "Screenresolution", "Fullscreen", "Graphics"],
         "Language":   ["back", "German", "English"],
-        "Graphics":   ["back", "Ines' design", "Julia's design"],
+        "Graphics":   ["back", "Ines' design", "Julia's design", "Julia's design 2"],
         "Resolution": ["back"],
         "Fullscreen": ["back", "True", "False"],
         "Game speed": ["back", "Slow", "Normal", "Fast", "Really fast"]
-        }
-          
-          
-menu_d = {"main":       ["Play", "Tutorial", "Levels", "Kampanie", "Hilfe", "Credits", "Settings","Ende das Spiel"],
-        "Tutorial":   ["zurück", "Tutorial 1", "Tutorial 2", "Tutorial 3", "Tutorial 4", "Tutorial 5"],
-        "Levels":     ["zurück", "Level 1-4", "Level 5-9", "Level 10-14", "Level 15-19", "Level 20-25"],
-        "Level 1-4":  ["zurück", "Tutorial", "Level 1", "Level 2", "Level 3", "Level 4"],
-        "Level 5-9":  ["zurück", "Level 5", "Level 6", "Level 7", "Level 8", "Level 9"], 
-        "Level 10-14":["zurück", "Level 10", "Level 11", "Level 12", "Level 13", "Level 14"],
-        "Level 15-19":["zurück", "Level 15", "Level 16", "Level 17", "Level 18", "Level 19"],
-        "Level 20-25":["zurück", "Level 20", "Level 21", "Level 22", "Level 23", "Level 24", "Level 25"],
-        "Campaign":   ["zurück", "Mission 1", "Mission 2", "Mission 3", "Mission 4", "Mission 5"],
-        "Help":       ["zurück", "Hauptinsel", "Holzinsel", "Eiseninsel", "Schiffinsel",],
-        "Credits":    ["zurück", "Ines Schnabl", "Martin Schnabl", "Julia Steger","Marcel Korinek"],
-        "Settings":   ["zurück", "Sprache", "Spiel Geschwindigkeit", "Screenresolution", "Fullscreen", "Graphics"],
-        "Sprache":    ["zurück", "Englisch", "Deutsch"],
-        "Graphics":   ["zurück", "Ines' design", "Julia's design"],
-        "Resolution": ["zurück"],
-        "Fullscreen": ["zurück", "Ja", "Nein"],
-        "Spiel Geschwindigkeit": ["zurück", "Langsam", "Normal", "Schnell", "Sehr Schnell"]
         }
           
 descr_e = {"Resume"      :"Resume to the game",
@@ -55,32 +35,62 @@ descr_e = {"Resume"      :"Resume to the game",
          "Ship island"   :"On this island there is a shipyard that will produce a new ship whenever you have 5 pieces of wood and 5 ingots of iron.",
          "Campaign"      :"Not yet written. If you are good at storywriting, please contact us!",
          "Graphics"      :"Change the style of the game.",
-         }
+         }          
+         
+         
+         
+         
+         
+          
+menu_d = {"main":       ["Play", "Tutorial", "Levels", "Kampagne", "Hilfe", "Entwickler", "Einstellungen","Beenden"],
+        "Tutorial":   ["zurück", "Tutorial 1", "Tutorial 2", "Tutorial 3", "Tutorial 4", "Tutorial 5"],
+        "Levels":     ["zurück", "Level 1-4", "Level 5-9", "Level 10-14", "Level 15-19", "Level 20-25"],
+        "Level 1-4":  ["zurück", "Tutorial", "Level 1", "Level 2", "Level 3", "Level 4"],
+        "Level 5-9":  ["zurück", "Level 5", "Level 6", "Level 7", "Level 8", "Level 9"], 
+        "Level 10-14":["zurück", "Level 10", "Level 11", "Level 12", "Level 13", "Level 14"],
+        "Level 15-19":["zurück", "Level 15", "Level 16", "Level 17", "Level 18", "Level 19"],
+        "Level 20-25":["zurück", "Level 20", "Level 21", "Level 22", "Level 23", "Level 24", "Level 25"],
+        "Kampagne":   ["zurück", "Mission 1", "Mission 2", "Mission 3", "Mission 4", "Mission 5"],
+        "Hilfe":       ["zurück", "Hauptinsel", "Holzinsel", "Eiseninsel", "Schiffswerft",],
+        "Entwickler":    ["zurück", "Ines Schnabl", "Martin Schnabl", "Julia Steger","Marcel Korinek"],
+        "Einstellungen":   ["zurück", "Sprache", "Spielgeschwindigkeit", "Auflösung", "Vollbildschirm", "Grafik"],
+        "Sprache":    ["zurück", "Englisch", "Deutsch"],
+        "Graphics":   ["zurück", "Ines Entwurf", "Julias Entwurf", "Julias 2.Entwurf"],
+        "Auflösung": ["zurück"],
+        "Fullscreen": ["zurück", "Ja", "Nein"],
+        "Spielgeschwindigkeit": ["zurück", "Langsam", "Normal", "Schnell", "Sehr schnell"]
+        }
+          
             
 descr_d = {"Zurück"      :"Zurück zum Spiel",
          "Martin Schnabl":"Der Programmierer dieses Spiels.",
-         "Ines Schnabl"  :"Martins jüngere Schwester und verantwortlich für NIX",
-         "Julia Steger"  :"Verantwortlich fürs Spieltesten, die Graphiken, Verbesserungsvorschläge und Fehlersuche",
-         "Marcel Korinek":"Testen.",
-         "Settings"      :"Verändere die Screenresolution nur am Anfang!",
+         "Ines Schnabl"  :"Martins jüngere Schwester und verantwortlich für Grafik, Programmieren und Testen.",
+         "Julia Steger"  :"Verantwortlich fürs Spieltesten, die Grafiken, Verbesserungsvorschläge und Fehlersuche.",
+         "Marcel Korinek":"Verantwortlich für Testen, Fehlersuche und Weiterentwicklung.",
+         "Settings"      :"Verändere die Auflösung nur am Anfang!",
          "Tutorial 1"    :"Eine Einführung in die Grundprinzipien des Spiels.",
          "Tutorial 2"    :"Kompliziertere Verwaltung deiner Schiffe",
-         "Tutorial 3"    :"Teaches you how to build ships in the shipyard.Bringt dir bei wie man Schiffe in der Werft baut.",
+         "Tutorial 3"    :"Bringt dir bei wie man Schiffe in der Werft baut.",
          "Tutorial 4"    :"Dein erstes Zusammentreffen mit gegnerischen Schiffen",
-         "Tutorial 5"    :"Bringt dir die Strategien bei um das Spiel zugewinnen",
-         "Tutorial"      :"Lerne alles was du brauchst um das Spiel zu spielen",
-         "Hauptinsel"    :"Das ist die Insel wo am Anfang die meisten deiner Schiffe stationiert sind. Im späteren Spiel hat es (noch) keinen richtigen Zwech, aber du kannst sie nutzen um den Gegner abzulenken.",
+         "Tutorial 5"    :"Bringt dir Strategien bei, um das Spiel zu gewinnen",
+         "Tutorial"      :"Lerne alles was du brauchst, um das Spiel zu spielen",
+         "Hauptinsel"    :"Das ist die Insel auf der am Anfang die meisten deiner Schiffe stationiert sind. Im späteren Spiel hat es (noch) keinen richtigen Zweck, aber du kannst sie nutzen um den Gegner abzulenken.",
          "Holzinsel"     :"Wenn du diese Insel besitzt bekommst du jede Sekunde Holz",
          "Eiseninsel"    :"Wenn du diese Insel besitzt bekommst du jede Sekunde Eisen",
          "Schiffinsel"   :"Auf dieser Insel ist eine Werft die ein neues Schiff produzieren wird immer wenn du 5 Stücke Holz und 5 Barren Eisen hast.",
-         "Campaign"      :"Not yet written. If you are good at storywriting, please contact us!Noch nicht geschrieben. Wenn die ein_e gute_r Geschichtenschreiber_in bist kontaktiere uns!",
-         "Graphics"      :"Verändere den Style des Spiels",
+         "Kampagne"      :"Wenn die ein_e gute_r Geschichtenschreiber_in bist kontaktiere uns!",
+         "Grafik"      :"Verändere das Aussehen des Spiels",
          }
 
 menu_images = {"Main island" : "main_island",
                "Wood island" : "wood_island",
                "Iron island" : "iron_island",
                "Ship island" : "ship_island",
+               
+               "Hauptinsel"  : "main_island",
+               "Holzinsel"   : "wood_island",
+               "Eiseninsel"  : "iron_island",
+               "Werftinsel"  : "ship_island",
                }
 linelength = 27
 history = ["main"]
